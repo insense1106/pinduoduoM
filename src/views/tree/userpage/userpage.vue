af5badddc24d7dd53dd2610d4e36f006aa357ca1
insense1106 <insense11061106@163.com>
2020-05-30 14:19:18 +0800

    change the cpt userpage

--- src/views/tree/userpage/userpage.vue
+++ src/views/tree/userpage/userpage.vue
@@ -24,7 +24,8 @@
     <!-- <h1>{{ $store.state.music.list_pay_pdd }}</h1> -->
     <div>
       <!-- <canvas id="canvas" width="100" height="200"></canvas> -->
-      <canvas id="canvas"></canvas>
+      <!-- <canvas id="canvas"></canvas> -->
+      <!-- <h1>delete canvas</h1> -->
     </div>
   </div>
 </template>
